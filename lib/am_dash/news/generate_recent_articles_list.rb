6c1f78863a6fe0f9aa7f98dc3f59613ba11f061a
Ben Downey <bd@bendowney.net>
2016-04-06 21:14:52 -0700

    adds context for grabbing news articles

---
+++ lib/am_dash/news/generate_recent_articles_list.rb
@@ -0,0 +1,78 @@
+require 'net/http'
+require 'JSON'
+require 'am_dash/cache_expiration'
+
+module AMDash
+  module News
+    class GenerateRecentArticlesList
+      include AMDash::CacheExpiration
+
+      def initialize(cache)
+        @cache = cache
+      end
+
+      def execute
+        payload = selected_articles
+
+        write_to_cache(payload.to_json)
+      end
+
+      private
+
+      attr_reader :cache
+
+      def selected_articles
+        all_articles.first(5).map do |article|
+          {
+            headline: article["headline"]["main"],
+            snippet: article["snippet"],
+            url: article["web_url"]
+          }
+        end
+      end
+
+      def all_articles
+        response = Net::HTTP.get_response(request_uri)
+        if response.code == "200"
+          response_body = JSON.parse(response.body)
+          response_body["response"]["docs"]
+        else
+          []
+        end
+      end
+
+      def request_uri
+        uri = URI("http://api.nytimes.com/svc/search/v2/articlesearch.json?")
+
+        params = {
+          "begin_date" =>  start_date,
+          "end_date" => end_date,
+          "sort" => "newest",
+          "api-key" => ENV["AM_DASH_NYT_KEY"]
+        }
+
+        uri.query = URI.encode_www_form(params)
+
+        uri
+      end
+
+      def start_date
+        yesterday = Date.today.prev_day
+        yesterday.strftime("%Y%m%d")
+      end
+
+      def end_date
+        Date.today.strftime("%Y%m%d")
+      end
+
+      def write_to_cache(payload)
+        cache.write(
+          "news",
+          payload,
+          FOUR_HOURS
+        )
+      end
+
+    end
+  end
+end
---
+++ spec/am_dash/news/generate_recent_articles_list_spec.rb
@@ -0,0 +1,65 @@
+require 'am_dash/news/generate_recent_articles_list'
+
+describe AMDash::News::GenerateRecentArticlesList do
+  let(:cache) { double(:cache) }
+  let(:net_http_mock) { double(:net_http_mock) }
+  let(:news_query_response) { double(:news_query_response, code: "200",  body: response_body) }
+  let(:response_body) do
+    "{\"response\":{\"meta\":{\"hits\":1922,\"time\":71,\"offset\":0},\"docs\":[{\"web_url\":\"http://www.nytimes.com/aponline/2016/04/06/us/ap-us-teacher-recruitment.html\",\"snippet\":\"The Hawaii Department of Education has been seeking out educators from the mainland to deal with the state's growing teacher shortage.\",\"lead_paragraph\":\"The Hawaii Department of Education has been seeking out educators from the mainland to deal with the state's growing teacher shortage.\",\"abstract\":null,\"print_page\":null,\"blog\":[],\"source\":\"AP\",\"multimedia\":[],\"headline\":{\"main\":\"Hawaii Looks to Mainland to Deal With Big Teacher Shortage\",\"print_headline\":\"Hawaii Looks to Mainland to Deal With Big Teacher Shortage\"},\"keywords\":[],\"pub_date\":\"2016-04-06T22:38:37Z\",\"document_type\":\"article\",\"news_desk\":\"None\",\"section_name\":\"U.S.\",\"subsection_name\":null,\"byline\":{\"person\":[],\"original\":\"By THE ASSOCIATED PRESS\",\"organization\":\"THE ASSOCIATED PRESS\"},\"type_of_material\":\"News\",\"_id\":\"5705c92538f0d86865ca52ef\",\"word_count\":\"381\",\"slideshow_credits\":null}]}}" 
+  end
+
+  subject { described_class.new(cache) }
+
+  before do
+    stub_const('ENV', {'AM_DASH_NYT_KEY' => 'asdf'})
+  end
+
+  it "pulls a bunch of data from a news API" do
+    today = Date.today.strftime("%Y%m%d")
+    yesterday = Date.today.prev_day.strftime("%Y%m%d")
+
+    uri = URI("http://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=#{yesterday}&end_date=#{today}&sort=newest&api-key=#{ENV["AM_DASH_NYT_KEY"]}")
+
+    allow(Net::HTTP).to receive(:get_response).with(uri).and_return(news_query_response)
+
+    article_collection = [{:headline=>"Hawaii Looks to Mainland to Deal With Big Teacher Shortage", :snippet=>"The Hawaii Department of Education has been seeking out educators from the mainland to deal with the state's growing teacher shortage.", :url=>"http://www.nytimes.com/aponline/2016/04/06/us/ap-us-teacher-recruitment.html"}]
+
+    expect(cache).to receive(:write).with(
+      "news",
+      article_collection.to_json,
+      described_class::FOUR_HOURS
+    )
+    subject.execute
+  end
+
+  context "failure" do
+    it "handles 400 error code response" do
+
+      response_to_bad_request = Net::HTTPResponse.new(1.0, 400, "Your request is bad and you should feel bad!")
+
+      allow(Net::HTTP).to receive(:get_response).and_return(response_to_bad_request)
+
+      expect(cache).to receive(:write).with(
+        "news",
+        [].to_json,
+        described_class::FOUR_HOURS
+      )
+      subject.execute
+    end
+
+    it "handles 500 error code response" do
+
+      response_from_broken_news_service = Net::HTTPResponse.new(1.0, 400, "Barf!")
+
+      allow(Net::HTTP).to receive(:get_response).and_return(response_from_broken_news_service)
+
+      expect(cache).to receive(:write).with(
+        "news",
+        [].to_json,
+        described_class::FOUR_HOURS
+      )
+      subject.execute
+    end
+  end
+
+end
